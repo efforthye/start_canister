a4dcc8c4e6dbec0401042fbc3a05330325e90a49
efforthye <efforthye@gmail.com>
2023-10-16 23:09:47 +0900

    feat: add message

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Canister, query, text, update, Void } from 'azle';
 
 // This is a global variable that is stored on the heap
-let message = '';
+let message = '나는 귀요미';
 
 export default Canister({
     // Query calls complete quickly because they do not go through consensus
@@ -12,6 +12,5 @@ export default Canister({
     // This is because they persist state changes and go through consensus
     setMessage: update([text], Void, (newMessage) => {
         message = newMessage; // This change will be persisted
-    })
+    }),
 });
-
